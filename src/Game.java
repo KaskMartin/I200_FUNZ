891d36c7b7fe2ce165616fd7d5849ba1e59feac5
Martin Kask <kask.martin@gmail.com>
2015-11-27 22:30:07 +0200

    MENU + GAME töötavad koos, taevast sajab liiga palju saia

--- src/Game.java
+++ src/Game.java
@@ -2,7 +2,6 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
