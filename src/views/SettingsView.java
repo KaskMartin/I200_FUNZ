9b6a2101834afe813ae95902bf20fa7b24b2de27
KerstiM <kerstim@gmail.com>
2015-12-24 21:24:12 +0200

    testin kasutajate pilte

--- src/views/SettingsView.java
+++ src/views/SettingsView.java
@@ -8,10 +8,7 @@ import javafx.geometry.VPos;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -28,217 +25,85 @@ public class SettingsView extends Pane {
         this.setWidth(800);
         Font theFontSmall = Font.font("Consolas", FontWeight.LIGHT, 14);
 
-    //public void start(Stage primaryStage) {
-        //primaryStage.setTitle("JavaFX Welcome");
-    GridPane grid = new GridPane();
-    grid.setAlignment(Pos.CENTER);
-    grid.setHgap(10);
-    grid.setVgap(10);
-    grid.setPadding(new Insets(25, 25, 25, 25));
+        //GridPane nuppude jaoks
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.setPadding(new Insets(20,20,20,20));
 
-    Text scenetitle = new Text("VALI MÄNGIJA \n\nMÄNGIJA 1");
-    scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-    grid.add(scenetitle, 0, 0);
-        Label valimangija1 = new Label("Vali mängija 1");
-            //grid.add(valimangija1, 1, 1);
-        Label valimangija2 = new Label("Vali mängija 2");
-            //grid.add(valimangija1, 3, 1);
+        Label mangija1Label = new Label("MÄNGIJA 1");
+        mangija1Label.setContentDisplay(ContentDisplay.TOP);
+        mangija1Label.setPadding(new Insets(5,5,5,250));
 
-        final ToggleGroup valikunupp1 = new ToggleGroup();
+        //RadioButton nupud mängija1 kuvapiltide jaoks
+        RadioButton mangija1 = new RadioButton("");
+        mangija1.setGraphic(new ImageView("images/kasutaja01v.png"));
+        mangija1.setSelected(true);
+        mangija1.setPadding(new Insets(5,5,5,200));
 
-        RadioButton var1 = new RadioButton("1");
-        var1.setToggleGroup(valikunupp1);
-        var1.setUserData("1");
-        var1.setFont(theFontSmall);
+        final RadioButton mangija2 = new RadioButton("");
+        mangija2.setGraphic(new ImageView("images/kasutaja03v.png"));
+        mangija2.setPadding(new Insets(5, 5, 5, 200));
 
-        RadioButton var2 = new RadioButton("2");
-        var2.setToggleGroup(valikunupp1);
-        var2.setUserData("2");
-        var2.setFont(theFontSmall);
+        RadioButton mangija3 = new RadioButton("");
+        mangija3.setGraphic(new ImageView("images/kasutaja03v.png"));
+        mangija3.setPadding(new Insets(5,5,5,200));
 
-        final ToggleGroup valikunupp2 = new ToggleGroup();
-        RadioButton var3 = new RadioButton("3");
-        var3.setToggleGroup(valikunupp2);
-        var3.setUserData("3");
-        var3.setFont(theFontSmall);
+        RadioButton mangija4 = new RadioButton("");
+        mangija4.setGraphic(new ImageView("images/kasutaja04v.png"));
+        mangija4.setPadding(new Insets(5,5,5,200));
 
-        RadioButton var4 = new RadioButton("4");
-        var4.setToggleGroup(valikunupp2);
-        var4.setUserData("4");
-        var4.setFont(theFontSmall);
+        ToggleGroup mangija1Valikud = new ToggleGroup();
+        mangija1.setToggleGroup(mangija1Valikud);
+        mangija2.setToggleGroup(mangija1Valikud);
+        mangija3.setToggleGroup(mangija1Valikud);
+        mangija4.setToggleGroup(mangija1Valikud);
 
-        Image kasutaja03 = new Image("kasutaja03.png");
-                new Image(SettingsView.class.getResourceAsStream("kasutaja03.png")));
-        grid.add(kasutaja03, 2, 1);
+        VBox paneForRadioButtons1 = new VBox(20);
+        paneForRadioButtons1.setPadding(new Insets(5,5,5,5));
+        pane.add(mangija1Label, 2, 1);
+        pane.add(mangija1, 2, 2);
+        pane.add(mangija2, 2, 3);
+        pane.add(mangija3, 2, 4);
+        pane.add(mangija4, 2, 5);
 
-        //valikunupp.selectedToggleProperty().addListener((observable, oldValue, newValue) ->
+        Label mangija2Label = new Label("MÄNGIJA 2");
+        mangija2Label.setContentDisplay(ContentDisplay.TOP);
+        mangija2Label.setPadding(new Insets(5,5,5,100));
 
-                grid.add(var1, 1, 2);
-                grid.add(var2, 3, 2);
-                grid.add(var3, 1, 3);
-                grid.add(var4, 3, 3);
+        //RadioButton nupud mängija2 kuvapiltide jaoks
+        RadioButton mangija5 = new RadioButton("");
+        mangija5.setGraphic(new ImageView("images/kasutaja01sv.png"));
+        mangija5.setSelected(true);
+        mangija5.setPadding(new Insets(5,5,5,70));
 
+        RadioButton mangija6 = new RadioButton("");
+        mangija6.setGraphic(new ImageView("images/kasutaja01sv.png"));
+        mangija6.setPadding(new Insets(5,5,5,70));
 
-        //var1.setToggleGroup(valikunupp);
-        //var2.setToggleGroup(valikunupp);
+        RadioButton mangija7 = new RadioButton("");
+        mangija7.setGraphic(new ImageView("images/kasutaja01sv.png"));
+        mangija7.setPadding(new Insets(5,5,5,70));
 
-        //valikunupp.getToggles().addAll(var1, var2);
+        RadioButton mangija8 = new RadioButton("");
+        mangija8.setGraphic(new ImageView("images/kasutaja01sv.png"));
+        mangija8.setPadding(new Insets(5,5,5,70));
 
+        ToggleGroup mangija2Valikud = new ToggleGroup();
+        mangija5.setToggleGroup(mangija2Valikud);
+        mangija6.setToggleGroup(mangija2Valikud);
+        mangija7.setToggleGroup(mangija2Valikud);
+        mangija8.setToggleGroup(mangija2Valikud);
 
-        //Label userName = new Label("User Name:");
-    //grid.add(userName, 0, 1);
+        VBox paneForRadioButtons2 = new VBox(20);
+        paneForRadioButtons2.setPadding(new Insets(5,5,5,5));
+        pane.add(mangija5, 3, 2);
+        pane.add(mangija6, 3, 3);
+        pane.add(mangija7, 3, 4);
+        pane.add(mangija8, 3, 5);
+        pane.add(mangija2Label, 3, 1);
 
-    //TextField userTextField = new TextField();
-    //grid.add(userTextField, 1, 1);
-
-    //Label pw = new Label("Password:");
-    //grid.add(pw, 0, 2);
-
-    //PasswordField pwBox = new PasswordField();
-    //grid.add(pwBox, 1, 2);
-
-    //Button btn = new Button("Sign in");
-    //HBox hbBtn = new HBox(10);
-    //hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-    //hbBtn.getChildren().add(btn);
-    //grid.add(hbBtn, 1, 4);
-
-    final Text actiontarget = new Text();
-    grid.add(actiontarget, 1, 6);
-        this.getChildren().addAll(grid);
-
-   // btn.setOnAction(new EventHandler<ActionEvent>() {
-
-        //@Override
-        //public void handle(ActionEvent e) {
-          //  actiontarget.setFill(Color.FIREBRICK);
-            //actiontarget.setText("Sign in button pressed");
-      //  }
-    //});
-
-    //Scene scene = new Scene(grid, 300, 275);
-    //primaryStage.setScene(scene);
-    //primaryStage.show();
+        this.getChildren().addAll(pane);
 
 
-    /**public GridPane addGridPane() {
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(0, 10, 0, 10));
-
-        // Category in column 2, row 1
-        Text category = new Text("Sales:");
-        category.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-        grid.add(category, 1, 0);
-
-        // Title in column 3, row 1
-        Text chartTitle = new Text("Current Year");
-        chartTitle.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-        grid.add(chartTitle, 2, 0);
-
-        // Subtitle in columns 2-3, row 2
-        Text chartSubtitle = new Text("Goods and Services");
-        grid.add(chartSubtitle, 1, 1, 2, 1);
-
-        // House icon in column 1, rows 1-2
-        ImageView imageHouse = new ImageView(
-                new Image(SettingsView.class.getResourceAsStream("images/kasutaja03.png")));
-        grid.add(imageHouse, 0, 0, 1, 2);
-
-        // Left label in column 1 (bottom), row 3
-        Text goodsPercent = new Text("Goods\n80%");
-        GridPane.setValignment(goodsPercent, VPos.BOTTOM);
-        grid.add(goodsPercent, 0, 2);
-
-        // Chart in columns 2-3, row 3
-        ImageView imageChart = new ImageView(
-                new Image(SettingsView.class.getResourceAsStream("images/kasutaja03.png")));
-        grid.add(imageChart, 1, 2, 2, 1);
-
-        // Right label in column 4 (top), row 3
-        Text servicesPercent = new Text("Services\n20%");
-        GridPane.setValignment(servicesPercent, VPos.TOP);
-        grid.add(servicesPercent, 3, 2);
-
-        this.getChildren().addAll(grid);
-        return grid;
-
-       /** GridPane settinguGrid = new GridPane();
-        Image mangija01= new Image("images/kasutaja03.png");
-        ImageView mangija1 = new ImageView("images/kasutaja03.png");
-        settinguGrid.add(mangija1, 2, 2);
-
-        this.getChildren().addAll(settinguGrid, mangija1);
-
-
-        Label settingsInfo1 = new Label("VALI MÄNGIJA \n\nMÄNGIJA 1");
-        settingsInfo1.setTranslateX(100);
-        settingsInfo1.setTranslateY(60);
-        settingsInfo1.setFont(theFontSmall);
-        Label settingsInfo2 = new Label("VALI MÄNGIJA \n\nMÄNGIJA 2");
-        settingsInfo2.setTranslateX(475);
-        settingsInfo2.setTranslateY(60);
-        settingsInfo2.setFont(theFontSmall);
-        this.getChildren().addAll(settingsInfo1, settingsInfo2);
-
-        final ToggleGroup valikunupp = new ToggleGroup();
-
-        RadioButton var1 = new RadioButton("");
-        var1.setToggleGroup(valikunupp);
-        var1.setUserData("");
-        var1.setFont(theFontSmall);
-
-        RadioButton var2 = new RadioButton("");
-        var2.setToggleGroup(valikunupp);
-        var2.setUserData("");
-        var2.setFont(theFontSmall);
-
-        RadioButton var3 = new RadioButton("");
-        var3.setToggleGroup(valikunupp);
-        var3.setUserData("");
-        var3.setFont(theFontSmall);
-
-        RadioButton var4 = new RadioButton("");
-        var4.setToggleGroup(valikunupp);
-        var4.setUserData("");
-        var4.setFont(theFontSmall);
-
-        valikunupp.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
-
-
-        });
-
-        HBox hbox = new HBox();
-        HBox hbox2 = new HBox();
-        VBox vbox = new VBox();
-        VBox vbox2 = new VBox();
-
-        vbox.getChildren().add(var1);
-        vbox.getChildren().add(var2);
-        vbox.setSpacing(80);
-        vbox.setTranslateX(70);
-        vbox.setTranslateY(0);
-
-        vbox2.getChildren().add(var3);
-        vbox2.getChildren().add(var4);
-        vbox2.setSpacing(80);//valikunuppude omavaheline kaugus
-        vbox2.setTranslateX(70);
-        vbox2.setTranslateY(50);
-
-        hbox.getChildren().add(vbox);
-        hbox.setSpacing(150);
-        hbox.setPadding(new Insets(50, 10, 10, 20));
-        hbox.setTranslateX(50);
-        hbox.setTranslateY(100);
-
-        hbox2.getChildren().add(vbox2);
-        hbox.setSpacing(150);
-        hbox.setPadding(new Insets(50, 10, 10, 20));
-        hbox2.setTranslateX(450);
-        hbox2.setTranslateY(100);
-
-        this.getChildren().addAll(hbox, hbox2);
-        */
     }
 }
